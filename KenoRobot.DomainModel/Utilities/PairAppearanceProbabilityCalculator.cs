542b83e3774bf8390d28e22e3430227c32940d21
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-01-16 23:24:24 +0200

    Added restriction on parameter.

--- KenoRobot.DomainModel/Utilities/PairAppearanceProbabilityCalculator.cs
+++ KenoRobot.DomainModel/Utilities/PairAppearanceProbabilityCalculator.cs
@@ -14,6 +14,12 @@ namespace KenoRobot.DomainModel.Utilities
         /// <returns>Probability.</returns>
         public double GetAppearanceProbability(int delay)
         {
+            if (delay < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "delay", "Delay should not be less than 0.");
+            }
+
             return 1.0 - Math.Pow(1.0 - 19.0 / 316.0, delay + 1);
         }
     }
